42c1fc073137b597234c79ae18b33e93bfb21550
Tongchai142 <65160211@go.buu.ac.th>
2024-04-08 18:01:28 +0700

    checkotp

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -24,6 +24,25 @@ class MailController extends Controller
         
         Mail::to($request->email)->send(new TestMail($request->email,$mailData));
 
-        echo('Email success.');
+
+        return view('enter_otp')->with('email', $request->email);
+
+
     }
+
+    public function verifyOTP(Request $request)
+    {
+        // ตรวจสอบว่า OTP ที่ผู้ใช้กรอกถูกต้องหรือไม่
+        if ($request->otp === $request->session()->get('otp')) {
+            // OTP ถูกต้อง ทำการลบ OTP ออกจาก Session
+            $request->session()->forget('otp');
+            // Redirect ไปหน้าที่ต้องการ
+            return redirect('/login');
+        } else {
+            // OTP ไม่ถูกต้อง ให้แสดงข้อความผิดพลาด
+            return back()->withErrors(['otp' => 'Incorrect OTP, please try again.']);
+        }
+    }
+
+    
 }
